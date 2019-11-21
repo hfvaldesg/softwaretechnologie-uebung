8f5736d4e5a025f8532564511fe07ccd926f2b1b
Hernán Valdés <hfvaldesg@gmail.com>
2019-11-21 09:35:00 +0100

    Change the folder structure in the project

--- uebung1/main.cpp
+++ uebung1/main.cpp
@@ -13,7 +13,6 @@ int main(){
     // Point p5 {2, 3};
     // Point p6 {6, 4};
     // Point p7 {1, 4};
-    
     // std::vector <Point> points {p1, p2, p3, p4, p5, p6, p7};   
     // ch_polygon(points, true);
     // ch_rectangle(points, true);
@@ -23,7 +22,7 @@ int main(){
     // Point p3 {8, 3};
     // Point p4 {5, 4};
     // Point p5 {4, 6};
-    
+
     // std::vector <Point> points2 {p1, p2, p3, p4, p5};   
     // ch_polygon(points2, true);
     // ch_rectangle(points2, true);
@@ -36,7 +35,7 @@ int main(){
     // Point p6 {-1, 1};
     // Point p7 {-1, -1};
     // Point p8 {1, -1};
-    
+
     // std::vector <Point> points {p1, p2, p3, p4, p5, p6, p7, p8};   
     // ch_polygon(points, true);
     // ch_rectangle(points, true);
@@ -56,7 +55,7 @@ int main(){
     Point p13 {4, 3};
     Point p14 {5, 5};
     Point p15 {7, 3};
-    
+
     std::vector <Point> points {
         p1, p2, p3, p4, p5, 
         p6, p7, p8, p9, p10,
